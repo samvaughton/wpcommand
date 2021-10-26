ad5f7134e0984092c5fcadbd8687367e1e3698f6
Sam Vaughton <samvaughton@gmail.com>
2021-10-26 12:18:26 +0100

    Remove unique constraint from name

--- migrations/000009_create_blueprint_sets_table.up.sql
+++ migrations/000009_create_blueprint_sets_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS blueprint_sets(
     id SERIAL PRIMARY KEY,
     account_id INT NOT NULL,
     uuid TEXT UNIQUE NOT NULL,
-    name TEXT UNIQUE NOT NULL,
+    name TEXT NOT NULL,
     enabled BOOLEAN,
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
